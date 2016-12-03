59d29725e381cb669ca749a21049dd6e7e337f56
Suet <styinglee@gmail.com>
2016-12-03 22:04:46 +0000

    update

--- app/get_user_alerts.py
+++ app/get_user_alerts.py
@@ -3,11 +3,10 @@ from app import app
 import config
 import MySQLdb
 import MySQLdb.cursors
-from flask import jsonify
 
-def get_user_alerts():
+def get_user_alerts(service, area):
 	db = MySQLdb.connect(host=app.DB_HOST, user=app.DB_USER, passwd=app.DB_PASSWD, db=app.DB_NAME, cursorclass=MySQLdb.cursors.DictCursor)
 	with closing(db.cursor()) as cursor:
-		cursor.execute("SELECT * FROM user_alerts WHERE active = 1")
-		data = jsonify(cursor.fetchall())
+		cursor.execute("SELECT * FROM user_alerts WHERE service = %s AND area = %s AND active = 1", [service, area])
+		data = cursor.fetchall()
 	return data
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, jsonify
 from get_user_alerts import get_user_alerts
+from create_alert import create_alert
 
 @app.route('/')
 @app.route('/index')
@@ -12,6 +13,10 @@ def index():
 def text(message='hi'):
 	return message
 
-@app.route('/api/get-user-alerts')
-def alerts():
-	return get_user_alerts()
+@app.route('/api/get-user-alerts/<service>/<area>')
+def alerts(service, area):
+	return jsonify(get_user_alerts(service, area))
+
+@app.route('/create-alert/<service>/<area>/<name>')
+def route_create_alert(service, area, name):
+	return create_alert(service, area, name)
