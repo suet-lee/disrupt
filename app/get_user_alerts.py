a9780a20ecca4109584f3c52320b5c448b610567
Matti Astrand <mattiastr@gmail.com>
2016-12-04 01:33:05 +0000

    switch to ESRI arcgis

--- app/get_user_alerts.py
+++ app/get_user_alerts.py
@@ -3,7 +3,7 @@ from app import app
 import config
 import MySQLdb
 import MySQLdb.cursors
-from geopy.geocoders import Nominatim
+from geopy.geocoders import ArcGIS
 
 def get_user_alerts(service, area):
 	db = MySQLdb.connect(host=app.DB_HOST, user=app.DB_USER, passwd=app.DB_PASSWD, db=app.DB_NAME, cursorclass=MySQLdb.cursors.DictCursor)
@@ -25,7 +25,7 @@ def save_service(content):
     address_string = content['address_line_1']+ content['address_line_2']+ content['postcode']+ content['town_city']+ content['country']
     address_string = '{address_line_1}, {address_line_2}, {postcode}, {town_city}, {country}'.format(**content)
     print address_string
-    geolocator = Nominatim()
+    geolocator = ArcGIS()
     loc = geolocator.geocode(address_string)
     print loc.raw
     db = MySQLdb.connect(host=app.DB_HOST, user=app.DB_USER, passwd=app.DB_PASSWD, db=app.DB_NAME, cursorclass=MySQLdb.cursors.DictCursor)
